a2f6abf77ccc3e05eaace82b03fddd5ab2d6913a
David Dinh <daviddinh93@gmail.com>
2024-12-03 17:15:02 +0000

    silly stylings

--- 03a-solution.js
+++ 03a-solution.js
@@ -1,5 +1,5 @@
 const regex = /mul\(\d+,\d+\)/gm
 let results = []
-while ((m = regex.exec(require("fs").readFileSync("./03-input.txt", "utf-8"))) !== null) 
+while ((m = regex.exec(require("fs").readFileSync("./03-input.txt", "utf-8"))) !== null)
   m.map((match) => {results.push(match.slice(4,-1).split(",").map(Number))})
 console.log(results.reduce((a,b) => a + (b[1] * b[0]), 0))
--- 03b-solution.js
+++ 03b-solution.js
@@ -1,10 +1,11 @@
-const regex = /mul\(\d+,\d+\)|don't\(\)|do\(\)/gm
-let matches = [...require("fs").readFileSync("./03-input.txt", "utf-8").matchAll(regex)]
-let results = []
-let includeResult = true
+results = []
+includeResult = true
+matches = [...require("fs").readFileSync("./03-input.txt", "utf-8")
+  .matchAll(/mul\(\d+,\d+\)|don't\(\)|do\(\)/gm)]
 matches.forEach((match) => {
   if (match[0].indexOf("don't()") !== -1) includeResult = false
   if (match[0].indexOf("do()") !== -1) includeResult = true
-  if ((match[0].indexOf("mul(") !== -1) && includeResult) results.push(match[0].slice(4,-1).split(",").map(Number))
-});
+  if ((match[0].indexOf("mul(") !== -1) && includeResult) 
+    results.push(match[0].slice(4,-1).split(",").map(Number))
+})
 console.log(results.reduce((a,b) => a + (b[1] * b[0]), 0))

c9de17139d4388a25a7384e11818ec72f44634e5
David Dinh <daviddinh93@gmail.com>
2024-12-03 10:08:05 +0000

    refactor just for joe

--- 03a-solution.js
+++ 03a-solution.js
@@ -1,7 +1,5 @@
 const regex = /mul\(\d+,\d+\)/gm
 let results = []
-while ((m = regex.exec(require("fs").readFileSync("./03-input.txt", "utf-8"))) !== null) {
-  if (m.index === regex.lastIndex) regex.lastIndex++
+while ((m = regex.exec(require("fs").readFileSync("./03-input.txt", "utf-8"))) !== null) 
   m.map((match) => {results.push(match.slice(4,-1).split(",").map(Number))})
-}
 console.log(results.reduce((a,b) => a + (b[1] * b[0]), 0))

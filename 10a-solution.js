1d95cdc523248883f6582f6f7df0e088a6a2c56c
David Dinh <daviddinh93@gmail.com>
2024-12-13 16:55:03 +0000

    day 10 complete

---
+++ 10a-solution.js
@@ -0,0 +1,40 @@
+let input = require("fs")
+  .readFileSync("./10-input.txt", "utf-8")
+  .split("\n").map(e => e.split('').map(Number))
+
+let trailheads = input.map((row, rID) => 
+  row.map((col,cID) => {
+    if (col == 0) return [rID,cID]
+  })).flat().filter(e => e)
+
+const nextPossibleSteps = (p) => {
+  let currentH = input[p[0]]?.[p[1]]
+  let nextSteps = []
+  if (input[p[0]]?.[p[1]-1] == (currentH + 1)) nextSteps.push([p[0],p[1]-1])
+  if (input[p[0]-1]?.[p[1]] == (currentH + 1)) nextSteps.push([p[0]-1,p[1]])
+  if (input[p[0]]?.[p[1]+1] == (currentH + 1)) nextSteps.push([p[0],p[1]+1])
+  if (input[p[0]+1]?.[p[1]] == (currentH + 1)) nextSteps.push([p[0]+1,p[1]])
+  return nextSteps
+}
+
+let possibleRoutes = trailheads.map(e => {
+  let routes = new Array()
+  routes.push([e])
+  let i = 0 
+  while(i < 9) {
+    routes = routes.map(route => {
+        let nextSteps = nextPossibleSteps(route.at(-1))
+        if (nextSteps.length > 0) {
+          return nextSteps.map(f => route.slice().concat([f]))
+        }
+        return [route]
+    }).flat()
+    i++
+  }
+  return routes
+})
+
+console.log(possibleRoutes
+  .map(e => new Set(e.filter(f => f.length > 9).map(f => f[9].join('-'))).size)
+  .reduce((a,b) => a + b,0 )
+)
